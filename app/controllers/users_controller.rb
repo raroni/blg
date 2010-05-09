277c4a3e2d75e11b3a97f1392cb79b17137538bf
rasmusrn <rasmusrnielsen@gmail.com>
2010-05-09 20:32:52 +0200

    Very basic user creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.scoped
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      redirect_to users_path, :notice => 'User created.'
+    else
+      render :action => 'new'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def password
+  end
+  
+  def password=(password)
+    self.password_salt = Digest::SHA1.hexdigest Time.now.to_s
+    self.encrypted_password = Digest::SHA1.hexdigest password_salt + password
+  end
 end
