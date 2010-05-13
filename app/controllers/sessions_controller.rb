27c93ea364f0bb07352b00d6a3e3b0f759ffa662
rasmusrn <rasmusrnielsen@gmail.com>
2010-05-13 16:02:52 +0200

    Logout functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
     @user = User.find_by_email params[:email]
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to '/something'
+      redirect_to root_path, :notice => "You're now logged in."
     else
       render :action => :new
     end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, :notice => "You're now logged out."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Blg::Application.routes.draw do |map|
   resources :users, :except => [:show, :destroy]
   
   get 'login', :to => 'sessions#new'
+  get 'logout', :to => 'sessions#destroy'
   post 'sessions', :to => 'sessions#create'
   
   root :to => 'posts#index'
